4ee17e152461dedffd2f97af588781a04f994d08
Patryk Krawczyk <patrick36212@gmail.com>
2023-03-20 20:04:22 +0100

    create basic empty component with working data fetching

--- src/features/Forecast/index.js
+++ src/features/Forecast/index.js
@@ -1,9 +1,38 @@
 import Header from "../../components/Header";
-import DateComponent from "../../components/DateComponent";
 import Main from "../../components/Main";
 import Section from "../../components/Section";
+import { useQuery } from "react-query";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectSearchValues,
+  setSearch,
+} from "../../components/Search/searchSlice";
+import { getForecastData } from "./getForecastData";
+import { useLocation } from "react-router";
+import { toForecast } from "../../core/routes";
+import React, { useEffect } from "react";
+import { setCity } from "./forecastSlice";
+import DateComponent from "../../components/DateComponent";
 
 const Forecast = () => {
+  const location = useLocation();
+  const searchValues = useSelector(selectSearchValues);
+  const dispatch = useDispatch();
+
+  const forecastData = useQuery(["forecastData", { searchValues }], () => {
+    if (!!searchValues && location.pathname === toForecast) {
+      const stringifyCoordinates = `${searchValues.lat.toString()},${searchValues.lon.toString()}`;
+      return getForecastData(stringifyCoordinates);
+    }
+  });
+
+  useEffect(() => {
+    if (!!forecastData.data) {
+      dispatch(setCity(forecastData.data));
+      dispatch(setSearch(null));
+    }
+  }, [dispatch, searchValues, forecastData]);
+
   return (
     <>
       <Header />

18f7251b09157c2ecbb3031cfd06f5efbbc02211
Patryk Krawczyk <patrick36212@gmail.com>
2023-04-06 16:56:31 +0200

    delete routes to forecast

--- src/core/routes.js
+++ src/core/routes.js
@@ -5,16 +5,13 @@ import {
   Route,
 } from "react-router-dom";
 import Current from "../features/Current";
-import Forecast from "../features/Forecast";
 
 export const toCurrentWeather = "/current";
-export const toForecast = "/forecast";
 
 export const router = createHashRouter(
   createRoutesFromElements(
     <>
       <Route path={toCurrentWeather} element={<Current />} />
-      <Route path={toForecast} element={<Forecast />} />
       <Route path="/" element={<Navigate to={toCurrentWeather} />} />
     </>
   )

bb602c4c577f7d55ec996eed7b0bc67b491a4f8a
Patryk Krawczyk <patrick36212@gmail.com>
2023-04-07 11:27:10 +0200

    create section component

---
+++ src/components/Section/index.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Section = styled.section`
+  display: inherit;
+  flex-grow: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default Section;

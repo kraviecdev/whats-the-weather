7a6f13cba1dfda32307279e19e380149043fb661
Patryk Krawczyk <patrick36212@gmail.com>
2023-04-11 19:52:12 +0200

    made basic first screen

---
+++ src/features/FirstScreen/index.js
@@ -0,0 +1,15 @@
+import Section from "../../components/Section";
+import Search from "../../components/Search";
+
+const FirstScreen = () => {
+  return (
+    <>
+      <Search />
+      <Section>
+        <h3>Search your first City</h3>
+      </Section>
+    </>
+  );
+};
+
+export default FirstScreen;

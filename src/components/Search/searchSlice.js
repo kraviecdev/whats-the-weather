58dd5fa338d4c6505c1be3965c6967c102849814
Patryk Krawczyk <patrick36212@gmail.com>
2023-04-12 20:09:49 +0200

    add fetching searches from localStorage

--- src/components/Search/searchSlice.js
+++ src/components/Search/searchSlice.js
@@ -1,24 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getSearchesFromLocalStorage } from "../../core/saveInLocalStorage";
 
 const searchSlice = createSlice({
   name: "search",
   initialState: {
     searchValues: null,
-    searches: [],
+    doneSearches: getSearchesFromLocalStorage(),
   },
   reducers: {
     setSearch: (state, { payload: cityInfo }) => {
       state.searchValues = cityInfo;
     },
-    setSearches: (state, { payload: searches }) => {
-      state.searches.push(searches);
+    setDoneSearches: (state, { payload: search }) => {
+      state.doneSearches.push(search);
+    },
+    toggleSearchToFavourite: (state, { payload: searchId }) => {
+      const savedSearchIndex = state.doneSearches.findIndex(
+        ({ id }) => id === searchId
+      );
+      state.doneSearches[savedSearchIndex].fav =
+        !state.doneSearches[savedSearchIndex].fav;
     },
   },
 });
 
-export const { setSearches, setSearch } = searchSlice.actions;
+export const { setDoneSearches, setSearch, toggleSearchToFavourite } =
+  searchSlice.actions;
 
 export const selectSearchValues = (state) => state.search.searchValues;
-export const selectSearches = (state) => state.search.searches;
+export const selectDoneSearches = (state) => state.search.doneSearches;
+export const selectFavouriteSearches = (state) =>
+  state.search.favouriteSearches;
 
 export default searchSlice.reducer;

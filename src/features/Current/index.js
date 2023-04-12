a265684c9202e4b8e54228a2d0ff21b3a2c04b77
Patryk Krawczyk <patrick36212@gmail.com>
2023-04-12 20:11:44 +0200

    add functionality to adding favourites and saving them in localstorage

--- src/features/Current/index.js
+++ src/features/Current/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { useDispatch, useSelector } from "react-redux";
 import { getData } from "./getData";
@@ -7,15 +7,24 @@ import {
   setCityWeather,
   setHourlyWeather,
 } from "./currentSlice";
-import { selectSearchValues } from "../../components/Search/searchSlice";
+import {
+  selectDoneSearches,
+  selectSearchValues,
+  toggleSearchToFavourite,
+} from "../../components/Search/searchSlice";
 import WeatherTile from "../../components/WeatherTile";
 import { LoaderIcon } from "../../components/StatusInfo/Loading/styled";
 import Section from "../../components/Section";
+import Search from "../../components/Search";
+import { saveSearchesInLocalStorage } from "../../core/saveInLocalStorage";
 
 const Current = () => {
   const dispatch = useDispatch();
   const searchValues = useSelector(selectSearchValues);
   const cityWeather = useSelector(selectCityWeather);
+  const doneSearches = useSelector(selectDoneSearches);
+
+  const [isFavourite, setIsFavourite] = useState(false);
 
   const { data, isLoading } = useQuery(
     ["cityWeather", { searchValues }],
@@ -28,21 +37,42 @@ const Current = () => {
   );
 
   useEffect(() => {
+    if (doneSearches.length > 0) {
+      const searchIndex = doneSearches.findIndex(
+        ({ id }) => id === searchValues.id
+      );
+      doneSearches[searchIndex].fav === true
+        ? setIsFavourite(true)
+        : setIsFavourite(false);
+    }
+    saveSearchesInLocalStorage(doneSearches);
+  }, [doneSearches, searchValues]);
+
+  useEffect(() => {
     if (!!data) {
-      const hourlyDay = data.forecast.forecastday[0].hour;
-      const currentHour = new Date(
-        data.location.localtime_epoch * 1000
-      ).getHours();
+      const currentDay = data.forecast.forecastday[0].hour;
+      const nextDay = data.forecast.forecastday[1].hour;
+      const hourly = [].concat(currentDay, nextDay);
+      const currentHour = data.location.localtime.split(" ")[1].split(":")[0];
       dispatch(setCityWeather(data));
-      dispatch(setHourlyWeather({ hourly: hourlyDay, index: currentHour }));
+      dispatch(setHourlyWeather({ hourly: hourly, index: currentHour }));
     }
   }, [data, dispatch]);
 
   return (
-    <Section>
-      {isLoading && <LoaderIcon />}
-      {!!cityWeather && !isLoading && <WeatherTile data={cityWeather} />}
-    </Section>
+    <>
+      <Search />
+      <Section>
+        {isLoading && <LoaderIcon />}
+        {!!cityWeather && !isLoading && (
+          <WeatherTile
+            data={cityWeather}
+            saveInFav={() => dispatch(toggleSearchToFavourite(searchValues.id))}
+            isAddedToFav={isFavourite}
+          />
+        )}
+      </Section>
+    </>
   );
 };
 

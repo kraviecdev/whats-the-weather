5e8d5189e81b1c9a4644d602c3b2d51d433c8dd2
Patryk Krawczyk <patrick36212@gmail.com>
2023-04-12 20:10:12 +0200

    add dependency to FirstScreen

--- src/features/FirstScreen/index.js
+++ src/features/FirstScreen/index.js
@@ -1,7 +1,22 @@
 import Section from "../../components/Section";
 import Search from "../../components/Search";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectDoneSearches,
+  setSearch,
+} from "../../components/Search/searchSlice";
+import React from "react";
+import { Navigate } from "react-router-dom";
 
 const FirstScreen = () => {
+  const doneSearches = useSelector(selectDoneSearches);
+  const dispatch = useDispatch();
+
+  if (doneSearches.length > 0) {
+    dispatch(setSearch(doneSearches[0]));
+    return <Navigate to={`/weather/${doneSearches[0].name}`} />;
+  }
+
   return (
     <>
       <Search />

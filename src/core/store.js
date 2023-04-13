94e5218a3b251fab32f7ed79a05232b1b61b8f2a
Patryk Krawczyk <patrick36212@gmail.com>
2023-04-13 13:37:18 +0200

    implement currentPosition reducer

--- src/core/store.js
+++ src/core/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchReducer from "../components/Search/searchSlice";
 import currentReducer from "../features/Current/currentSlice";
+import currentPositionReducer from "../features/CurrentPositionWeather/currentPositionSlice";
 
 const store = configureStore({
   reducer: {
     search: searchReducer,
     current: currentReducer,
+    currentPosition: currentPositionReducer,
   },
 });
 

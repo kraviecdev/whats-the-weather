10e1ca6b529d96449a050a63281df22146451aa2
Patryk Krawczyk <patrick36212@gmail.com>
2023-04-13 13:40:24 +0200

    names refactor and implement

--- src/core/routes.js
+++ src/core/routes.js
@@ -5,17 +5,20 @@ import {
   Route,
 } from "react-router-dom";
 import Current from "../features/Current";
-import FirstScreen from "../features/FirstScreen";
+import CurrentPositionWeather from "../features/CurrentPositionWeather";
 
-export const toFirstScreen = "/weather";
+export const toCurrentPositionWeather = "/weather";
 const toCurrentCityWeather = "/weather/:city";
 
 export const router = createHashRouter(
   createRoutesFromElements(
     <>
-      <Route path={toFirstScreen} element={<FirstScreen />} />
       <Route path={toCurrentCityWeather} element={<Current />} />
-      <Route path="/" element={<Navigate to={toFirstScreen} />} />
+      <Route
+        path={toCurrentPositionWeather}
+        element={<CurrentPositionWeather />}
+      />
+      <Route path="/" element={<Navigate to={toCurrentPositionWeather} />} />
     </>
   )
 );

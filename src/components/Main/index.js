40d1ed5da1714c4a8b0c5ef9a5170d07419e78a1
Patryk Krawczyk <patrick36212@gmail.com>
2023-04-16 18:20:22 +0200

    adjust css to figma project

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -4,13 +4,13 @@ const Main = styled.main`
   font-family: "Inter", sans-serif;
   font-size: 16px;
   font-weight: 400;
-  letter-spacing: 0.05em;
+  letter-spacing: 0.001em;
   max-width: 1024px;
-  height: 100vh;
+  height: 100%;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
-  gap: 30px;
+  gap: 16px;
 `;
 
 export default Main;

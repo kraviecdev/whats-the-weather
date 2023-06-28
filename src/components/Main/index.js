ba030dc47ae5e2d891dcffc1a2c4ff71288e6bc9
Patryk Krawczyk <patrick36212@gmail.com>
2023-06-28 18:53:16 +0200

    del global style code

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,10 +1,6 @@
 import styled from "styled-components";
 
 const Main = styled.main`
-  font-family: "Inter", sans-serif;
-  font-size: 16px;
-  font-weight: 400;
-  letter-spacing: 0.001em;
   max-width: 1024px;
   height: 100%;
   margin: 0 auto;

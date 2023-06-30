32ed7ffa4ee4090bddee323eeee64e62f605353b
Patryk Krawczyk <patrick36212@gmail.com>
2023-06-30 13:49:05 +0200

    change 2 similar slices to one bigger

--- src/core/store.js
+++ src/core/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchReducer from "../components/Search/searchSlice";
-import currentReducer from "../features/Current/currentSlice";
-import currentPositionReducer from "../features/CurrentPositionWeather/currentPositionSlice";
+import weatherReducer from "../features/weatherSlice";
 
 const store = configureStore({
   reducer: {
     search: searchReducer,
-    current: currentReducer,
-    currentPosition: currentPositionReducer,
+    weather: weatherReducer,
   },
 });
 

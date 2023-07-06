b4a2eb4fe43c1a5f40cfd5fd70b3d5604d24f7e4
Patryk Krawczyk <patrick36212@gmail.com>
2023-07-06 20:52:29 +0200

    change vh to % for better ui

--- src/components/StatusInfo/styled.js
+++ src/components/StatusInfo/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const StatusWrapper = styled.div`
   display: flex;
-  height: 100vh;
+  height: 100%;
   flex-direction: column;
   justify-content: space-around;
   align-items: center;

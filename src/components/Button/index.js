6eaac907d389cc9be8afeb7feee8495bab0747a7
Patryk Krawczyk <patrick36212@gmail.com>
2023-07-07 17:25:43 +0200

    add css for forecast button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,9 +2,8 @@ import { ButtonLink, UpIcon } from "./styled";
 
 const Button = ({ handleOnClick, path, name, forecast }) => {
   return (
-    <ButtonLink to={path} onClick={handleOnClick}>
-      {name}
-      {forecast && <UpIcon />}
+    <ButtonLink to={path} onClick={handleOnClick} forecast={forecast}>
+      {name} <UpIcon />
     </ButtonLink>
   );
 };
--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -1,19 +1,46 @@
 import { ReactComponent as VectorUp } from "./up.svg";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const ButtonLink = styled(NavLink)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 6px;
   padding: 12px 32px;
   border-radius: 16px;
   text-decoration: none;
-  text-align: center;
   font-weight: 700;
   box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.25);
   color: ${({ theme }) => theme.colors.mainFont};
   background: ${({ theme }) => theme.colors.secondaryColor};
-  max-width: max-content;
+  width: max-content;
+
+  svg {
+    display: none;
+  }
+
+  ${({ forecast }) =>
+    forecast &&
+    css`
+      font-weight: 400;
+      position: absolute;
+      padding: 16px;
+      border-radius: 0;
+      bottom: 0;
+      width: 100%;
+      box-shadow: 0px -4px 20px 0px rgba(0, 0, 0, 0.25);
+      background: linear-gradient(
+        180deg,
+        ${({ theme }) => theme.colors.secondaryColor} 0%,
+        ${({ theme }) => theme.colors.mainColor} 100%
+      );
+
+      svg {
+        display: block;
+      }
+    `}
 `;
 export const UpIcon = styled(VectorUp)`
-  width: 44px;
   fill: ${({ theme }) => theme.colors.mainFont};
 `;

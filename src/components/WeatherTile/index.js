2cd980ab640d0b00f644def31a6bdeaf4f9e41b0
Patryk Krawczyk <patrick36212@gmail.com>
2023-07-13 18:22:03 +0200

    divide weather tile component for current & forecast

--- src/components/WeatherTile/index.js
+++ src/components/WeatherTile/index.js
@@ -1,87 +1,32 @@
 import {
   FavouriteIcon,
-  IconInfoWrapper,
-  MainWrapper,
-  Title,
   HeadingWrapper,
   Info,
-  InfoWrapper,
-  SpecialInfo,
-  HourlyWrapper,
+  MainWrapper,
+  Title,
 } from "./styled";
 import DateComponent from "../Date";
-import Icon from "../Icon";
-import Slider from "./Slider";
 
-const WeatherTile = ({ data, saveInFav, isAddedToFav, hourlyData }) => {
-  const localtime = new Date(data.location.localtime).getTime();
-  const currentHour = new Date(localtime).getHours();
-
+const WeatherTile = ({
+  title,
+  favOnClick,
+  savedInFav,
+  localtime,
+  forecast,
+  children,
+}) => {
   return (
     <MainWrapper>
       <HeadingWrapper>
         <Title>
-          {data.location.name}{" "}
-          <FavouriteIcon onClick={saveInFav} added={isAddedToFav} />
+          {title}
+          {""}
+          <FavouriteIcon onClick={favOnClick} added={savedInFav} />
         </Title>
-        <DateComponent localtime={localtime} />
+        {localtime && <DateComponent localtime={localtime} />}
+        {forecast && <Info>14-day forecast</Info>}
       </HeadingWrapper>
-
-      <Slider />
-
-      <InfoWrapper>
-        <Info temperature="true">
-          {data.current.temp_c.toFixed(0)}
-          <SpecialInfo>&#176;C</SpecialInfo>
-        </Info>
-        <IconInfoWrapper>
-          <Icon
-            code={data.current.condition.code}
-            isDay={data.current.is_day}
-          />
-          <Info>{data.current.condition.text}</Info>
-        </IconInfoWrapper>
-      </InfoWrapper>
-
-      <InfoWrapper additionalWrapper="true">
-        <InfoWrapper additionalInfo="true">
-          <Info>
-            Feels like: <b>{data.current.feelslike_c.toFixed(0)}&#176;C</b>
-          </Info>
-          <Info>
-            Pressure: <b>{data.current.pressure_mb} hPA</b>
-          </Info>
-        </InfoWrapper>
-        <InfoWrapper additionalInfo="true">
-          <Info>
-            Humidify: <b>{data.current.humidity}%</b>
-          </Info>
-          <Info>
-            Wind speed: <b>{data.current.gust_kph} km/h</b>
-          </Info>
-        </InfoWrapper>
-      </InfoWrapper>
-
-      <InfoWrapper hourlyWrapper="true">
-        {hourlyData &&
-          hourlyData
-            .slice(currentHour, currentHour + 24)
-            .map((hourly, index) => (
-              <HourlyWrapper key={index}>
-                <Info first={index === 0}>
-                  {index === 0 ? "Now" : hourly.time.split(" ")[1]}
-                </Info>
-                <Icon
-                  hourly="true"
-                  code={hourly.condition.code}
-                  isDay={data.current.is_day}
-                />
-                <Info first={index === 0} hourly="true">
-                  {hourly.temp_c.toFixed(0)}&#176;C
-                </Info>
-              </HourlyWrapper>
-            ))}
-      </InfoWrapper>
+      {children}
     </MainWrapper>
   );
 };

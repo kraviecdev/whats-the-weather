40367346a3f08e21c1009c074939d43d79a31b34
Patryk Krawczyk <patrick36212@gmail.com>
2023-07-18 21:16:31 +0200

    add dependency for padding in different pages

--- src/features/index.js
+++ src/features/index.js
@@ -2,10 +2,13 @@ import Heading from "../components/Heading";
 import Search from "../components/Search";
 import Section from "../components/Section";
 import Main from "../components/Main";
+import { useLocation } from "react-router";
 
 const WeatherApp = ({ children, current }) => {
+  const location = useLocation();
+
   return (
-    <Main>
+    <Main forecast={location.pathname.split("/")[1] === "forecast"}>
       {current && (
         <>
           <Heading main="true" />

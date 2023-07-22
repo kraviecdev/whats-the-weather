c32d08f3c6781d56683b20a63dd012953e5db188
Patryk Krawczyk <patrick36212@gmail.com>
2023-07-22 19:21:19 +0200

    change props to one common data

--- src/components/WeatherTile/TempInfo/index.js
+++ src/components/WeatherTile/TempInfo/index.js
@@ -3,24 +3,21 @@ import { Info } from "../styled";
 import { TempInfoSpecial } from "./styled";
 import Icon from "../Icon";
 
-const TempInfo = ({
-  main,
-  secondary,
-  temperature,
-  conditionText,
-  iconCode,
-  isDay,
-}) => {
+const TempInfo = ({ main, secondary, conditionText, data }) => {
   return (
     <WeatherTileWrapper noGap={main} forecastInfoWrapper={secondary}>
       <Info biggestCenter={main} medium={secondary}>
-        {temperature}
+        {data.current.temp_c.toFixed(0)}
         {""}
         <TempInfoSpecial smallerBold={secondary}>&#176;C</TempInfoSpecial>
       </Info>
       <WeatherTileWrapper>
-        <Icon code={iconCode} isDay={isDay} forecast={secondary} />
-        {conditionText && <Info>{conditionText}</Info>}
+        <Icon
+          code={data.current.condition.code}
+          isDay={data.current.is_day}
+          forecast={secondary}
+        />
+        {conditionText && <Info>{data.current.condition.text}</Info>}
       </WeatherTileWrapper>
     </WeatherTileWrapper>
   );

fc972edbdbf336dac96f3e8b20b7fe41ef6408f1
Patryk Krawczyk <patrick36212@gmail.com>
2023-07-22 19:28:50 +0200

    create separate component for hourlyData

---
+++ src/components/WeatherTile/HourlyInfo/index.js
@@ -0,0 +1,23 @@
+import { WeatherTileSection } from "../WeatherTileSection";
+import { WeatherTileWrapper } from "../WeatherTileWrapper";
+import { Info } from "../styled";
+import Icon from "../Icon";
+
+const HourlyInfo = ({ hourlyData, currentHour }) => {
+  return (
+    <WeatherTileSection hourlyContentSection>
+      {hourlyData &&
+        hourlyData.slice(currentHour, currentHour + 24).map((hourly, index) => (
+          <WeatherTileWrapper hourlyInfoWrapper key={index}>
+            <Info larger={index === 0}>
+              {index === 0 ? "Now" : hourly.time.split(" ")[1]}
+            </Info>
+            <Icon hourly code={hourly.condition.code} isDay={hourly.is_day} />
+            <Info larger={index === 0}>{hourly.temp_c.toFixed(0)}&#176;C</Info>
+          </WeatherTileWrapper>
+        ))}
+    </WeatherTileSection>
+  );
+};
+
+export default HourlyInfo;

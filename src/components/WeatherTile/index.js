1647b4bcdd5015578b437af4129fe5a98aae3c6d
Patryk Krawczyk <patrick36212@gmail.com>
2023-07-22 21:49:15 +0200

    prop name change

--- src/components/WeatherTile/index.js
+++ src/components/WeatherTile/index.js
@@ -2,6 +2,7 @@ import { ArticleWrapper } from "./styled";
 import WeatherTileHeading from "./WeatherTileHeading";
 
 const WeatherTile = ({
+  tileView,
   title,
   favOnClick,
   savedInFav,
@@ -10,7 +11,7 @@ const WeatherTile = ({
   children,
 }) => {
   return (
-    <ArticleWrapper forecast={forecast}>
+    <ArticleWrapper tileView={tileView}>
       <WeatherTileHeading
         title={title}
         favOnClick={favOnClick}

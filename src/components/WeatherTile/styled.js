6be566dabfcbbab12f7a6bf41a59ca23eb7d9700
Patryk Krawczyk <patrick36212@gmail.com>
2023-07-22 19:35:16 +0200

    add section and wrapper component to common styled.js

--- src/components/WeatherTile/styled.js
+++ src/components/WeatherTile/styled.js
@@ -7,7 +7,88 @@ export const MainWrapper = styled.article`
   align-self: flex-start;
   width: 100%;
   color: ${({ theme }) => theme.colors.mainFont};
+
+  ${({ additionalArticleWrapper }) =>
+    additionalArticleWrapper &&
+    css`
+      flex-direction: row;
+      background: ${({ theme }) => theme.colors.secondaryColor};
+      border-radius: 16px;
+    `}
 `;
+
+export const WeatherTileSection = styled.section`
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  margin: 12px 0;
+
+  ${({ borderBottom }) =>
+    borderBottom &&
+    css`
+      border-bottom: 1px solid ${({ theme }) => theme.colors.secondaryFont};
+    `}
+
+  ${({ additionalContentSection }) =>
+    additionalContentSection &&
+    css`
+      grid-template-columns: repeat(2, 1fr);
+      gap: 0;
+    `}
+
+  ${({ hourlyContentSection }) =>
+    hourlyContentSection &&
+    css`
+      display: flex;
+      justify-content: flex-start;
+      overflow-x: auto;
+      gap: 0 34px;
+      padding: 0 0 12px;
+      margin: 12px auto;
+      max-width: 99%;
+    `}
+`;
+
+export const WeatherTileWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 8px;
+
+  ${({ noGap }) =>
+    noGap &&
+    css`
+      gap: 0;
+    `}
+
+  ${({ additionalInfoWrapper }) =>
+    additionalInfoWrapper &&
+    css`
+      margin: 12px 0;
+      align-items: flex-start;
+      justify-content: space-between;
+      border-right: 1px solid ${({ theme }) => theme.colors.secondaryFont};
+
+      &:last-of-type {
+        justify-self: right;
+        border-right: none;
+      }
+    `}
+
+  ${({ hourlyInfoWrapper }) =>
+    hourlyInfoWrapper &&
+    css`
+      min-width: max-content;
+    `}
+  
+  ${({ forecastInfoWrapper }) =>
+    forecastInfoWrapper &&
+    css`
+      flex-direction: row-reverse;
+    `}
+`;
+
 export const Info = styled.p`
   font-size: 14px;
   font-weight: 400;

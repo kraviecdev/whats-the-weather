63ee7bf7040bb5efa61dcccb4b8f7ae550af0bdf
Patryk Krawczyk <patrick36212@gmail.com>
2023-07-24 19:06:10 +0200

    add statement for data and navigate to "/" if no data returned

--- src/features/ForecastWeather/index.js
+++ src/features/ForecastWeather/index.js
@@ -3,15 +3,23 @@ import { useSelector } from "react-redux";
 import ForecastTile from "../../components/WeatherTile/ForecastTile";
 import { selectForecastData, selectWeatherData } from "../weatherSlice";
 import Button from "../../components/Button";
+import React from "react";
+import { Navigate } from "react-router-dom";
 
 const ForecastWeather = () => {
   const forecastData = useSelector(selectForecastData);
   const weatherData = useSelector(selectWeatherData);
 
+  if (!weatherData) {
+    return <Navigate to={"/"} />;
+  }
+
   return (
     <WeatherApp>
       <Button path={"/"} name={"Current weather"} forecast />
-      <ForecastTile data={weatherData} forecastData={forecastData} />
+      {!!weatherData && (
+        <ForecastTile data={weatherData} forecastData={forecastData} />
+      )}
     </WeatherApp>
   );
 };

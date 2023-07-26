50b1aafbc202fd2e7c06c8696a4d05de38961f83
Patryk Krawczyk <patrick36212@gmail.com>
2023-07-26 15:34:09 +0200

    create section for forecast

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Section = styled.section`
   display: inherit;
@@ -6,6 +6,24 @@ const Section = styled.section`
   justify-content: space-between;
   align-items: center;
   flex-direction: column;
+
+  ${({ forecastSection }) =>
+    forecastSection &&
+    css`
+      overflow: auto;
+      justify-content: flex-start;
+      height: 100%;
+      gap: 12px;
+      transform: translateY(-51px);
+      background: ${({ theme }) => theme.colors.mainColor};
+      transition: 0.7s ease-in-out;
+
+      ${({ activeSection }) =>
+        activeSection &&
+        css`
+          transform: translateY(-100vh);
+        `}
+    `}
 `;
 
 export default Section;

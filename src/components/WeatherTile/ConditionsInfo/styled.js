5266a5c4a22a57e9691474e5f38ba8031b7dd96d
Patryk Krawczyk <patrick36212@gmail.com>
2023-07-27 18:49:50 +0200

    import and style visibility icon

--- src/components/WeatherTile/ConditionsInfo/styled.js
+++ src/components/WeatherTile/ConditionsInfo/styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { ReactComponent as Humidify } from "./humidify.svg";
 import { ReactComponent as Wind } from "./wind.svg";
-import { ReactComponent as Pressure } from "./pressure.svg";
+import { ReactComponent as Visibility } from "./visibility.svg";
 
 export const HumidityIcon = styled(Humidify)`
   width: 12px;
@@ -15,7 +15,7 @@ export const WindIcon = styled(Wind)`
   fill: ${({ theme }) => theme.colors.mainFont};
 `;
 
-export const PressureIcon = styled(Pressure)`
+export const VisibilityIcon = styled(Visibility)`
   width: 12px;
   height: 12px;
   fill: ${({ theme }) => theme.colors.mainFont};

a3f8fade4383f5f0f917f518a493afecc8882e4e
Patryk Krawczyk <patrick36212@gmail.com>
2023-07-27 18:50:06 +0200

    change component structure for 2 variants

--- src/components/WeatherTile/ConditionsInfo/index.js
+++ src/components/WeatherTile/ConditionsInfo/index.js
@@ -1,5 +1,5 @@
 import { Info, WeatherTileWrapper, WeatherTileSection } from "../styled";
-import { HumidityIcon, PressureIcon, WindIcon } from "./styled";
+import { HumidityIcon, VisibilityIcon, WindIcon } from "./styled";
 
 const ConditionsInfo = ({
   basic,
@@ -9,53 +9,41 @@ const ConditionsInfo = ({
   pressure,
   humidity,
   wind,
+  visibility,
 }) => {
-  {
-    return (
-      <>
-        {basic && (
-          <WeatherTileSection additionalContentSection borderBottom>
-            <WeatherTileWrapper additionalInfoWrapper>
-              <Info>
-                Feels like: <b>{feelsLike}&#176;C</b>
-              </Info>
-              <Info>
-                Pressure: <b>{pressure} hPA</b>
-              </Info>
-            </WeatherTileWrapper>
+  return (
+    <WeatherTileSection additionalContentSection borderBottom={basic}>
+      {basic && (
+        <WeatherTileWrapper additionalInfoWrapper>
+          <Info>
+            Feels like: <b>{feelsLike}&#176;C</b>
+          </Info>
+          <Info>
+            Pressure: <b>{pressure} hPa</b>
+          </Info>
+        </WeatherTileWrapper>
+      )}
+      {secondary && (
+        <WeatherTileWrapper additionalInfoWrapper noMargin={secondary}>
+          {children}
+        </WeatherTileWrapper>
+      )}
 
-            <WeatherTileWrapper additionalInfoWrapper>
-              <Info>
-                Humidity: <b>{humidity}%</b>
-              </Info>
-              <Info>
-                Wind speed: <b>{wind} km/h</b>
-              </Info>
-            </WeatherTileWrapper>
-          </WeatherTileSection>
-        )}
+      <WeatherTileWrapper additionalInfoWrapper noMargin={secondary}>
         {secondary && (
-          <WeatherTileSection additionalContentSection>
-            <WeatherTileWrapper additionalInfoWrapper noMargin>
-              {children}
-            </WeatherTileWrapper>
-
-            <WeatherTileWrapper additionalInfoWrapper noMargin>
-              <Info>
-                <PressureIcon /> {pressure} hPA
-              </Info>
-              <Info>
-                <HumidityIcon /> {humidity}%
-              </Info>
-              <Info>
-                <WindIcon /> {wind} km/h
-              </Info>
-            </WeatherTileWrapper>
-          </WeatherTileSection>
+          <Info>
+            <VisibilityIcon /> <b>{visibility} km</b>
+          </Info>
         )}
-      </>
-    );
-  }
+        <Info>
+          {secondary ? <HumidityIcon /> : "Humidity:"} <b>{humidity}%</b>
+        </Info>
+        <Info>
+          {secondary ? <WindIcon /> : "Wind speed:"} <b>{wind} km/h</b>
+        </Info>
+      </WeatherTileWrapper>
+    </WeatherTileSection>
+  );
 };
 
 export default ConditionsInfo;

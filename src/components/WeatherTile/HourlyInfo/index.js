d3eb21b1c9780fb7d18093357930792e39eff2ce
Patryk Krawczyk <patrick36212@gmail.com>
2023-07-29 19:59:10 +0200

    unified hourly data

--- src/components/WeatherTile/HourlyInfo/index.js
+++ src/components/WeatherTile/HourlyInfo/index.js
@@ -2,16 +2,24 @@ import { Info, WeatherTileWrapper, WeatherTileSection } from "../styled";
 import Icon from "../TempInfo/IconInfo/Icon";
 
 const HourlyInfo = ({ hourlyData, currentHour }) => {
+  const hourly = currentHour
+    ? hourlyData.slice(currentHour, currentHour + 24)
+    : hourlyData;
+
   return (
     <WeatherTileSection hourlyContentSection>
       {hourlyData &&
-        hourlyData.slice(currentHour, currentHour + 24).map((hourly, index) => (
+        hourly.map((hourly, index) => (
           <WeatherTileWrapper hourlyInfoWrapper key={index}>
-            <Info larger={index === 0}>
-              {index === 0 ? "Now" : hourly.time.split(" ")[1]}
+            <Info larger={currentHour && index === 0 ? true : false}>
+              {currentHour && index === 0
+                ? "Now"
+                : `${hourly.time.split(" ")[1]}`}
             </Info>
             <Icon hourly code={hourly.condition.code} isDay={hourly.is_day} />
-            <Info larger={index === 0}>{hourly.temp_c.toFixed(0)}&#176;C</Info>
+            <Info larger={currentHour && index === 0 ? true : false}>
+              {hourly.temp_c.toFixed(0)}&#176;C
+            </Info>
           </WeatherTileWrapper>
         ))}
     </WeatherTileSection>

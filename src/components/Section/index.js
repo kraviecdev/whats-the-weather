9a3d128f5ef876215d81cf255055aa8c8d58f098
Patryk Krawczyk <patrick36212@gmail.com>
2023-07-30 18:08:55 +0200

    delete grow prop, change vh to dvh

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,8 +1,7 @@
 import styled, { css } from "styled-components";
 
 const Section = styled.section`
-  display: inherit;
-  flex-grow: 1;
+  display: flex;
   justify-content: space-between;
   align-items: center;
   flex-direction: column;
@@ -10,7 +9,6 @@ const Section = styled.section`
   ${({ forecastSection }) =>
     forecastSection &&
     css`
-      overflow: auto;
       justify-content: flex-start;
       height: 100%;
       gap: 12px;
@@ -21,7 +19,7 @@ const Section = styled.section`
       ${({ activeSection }) =>
         activeSection &&
         css`
-          transform: translateY(-100vh);
+          transform: translateY(-100dvh);
         `}
     `}
 `;

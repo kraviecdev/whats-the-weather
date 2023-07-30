e3a633c83c218ec0fcfeff9551bcba781cd21d3a
Patryk Krawczyk <patrick36212@gmail.com>
2023-07-30 17:22:22 +0200

    change info props statement, add prop for hourlyForecast

--- src/components/WeatherTile/HourlyInfo/index.js
+++ src/components/WeatherTile/HourlyInfo/index.js
@@ -1,23 +1,23 @@
 import { Info, WeatherTileWrapper, WeatherTileSection } from "../styled";
 import Icon from "../TempInfo/IconInfo/Icon";
 
-const HourlyInfo = ({ hourlyData, currentHour }) => {
+const HourlyInfo = ({ hourlyData, currentHour, hourlyForecast }) => {
   const hourly = currentHour
     ? hourlyData.slice(currentHour, currentHour + 24)
     : hourlyData;
 
   return (
-    <WeatherTileSection hourlyContentSection>
+    <WeatherTileSection hourlyContentSection hourlyForecast={hourlyForecast}>
       {hourlyData &&
         hourly.map((hourly, index) => (
           <WeatherTileWrapper hourlyInfoWrapper key={index}>
-            <Info larger={currentHour && index === 0 ? true : false}>
+            <Info larger={currentHour && index === 0}>
               {currentHour && index === 0
                 ? "Now"
                 : `${hourly.time.split(" ")[1]}`}
             </Info>
             <Icon hourly code={hourly.condition.code} isDay={hourly.is_day} />
-            <Info larger={currentHour && index === 0 ? true : false}>
+            <Info larger={currentHour && index === 0}>
               {hourly.temp_c.toFixed(0)}&#176;C
             </Info>
           </WeatherTileWrapper>

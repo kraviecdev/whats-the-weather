c5a484f4a08b28d90b858987dedc520eb29088f3
Patryk Krawczyk <patrick36212@gmail.com>
2023-07-30 17:24:39 +0200

    create styled tiles for forecast add data and fns to show/hide hourly data

--- src/components/WeatherTile/ForecastTile/index.js
+++ src/components/WeatherTile/ForecastTile/index.js
@@ -2,50 +2,69 @@ import WeatherTile from "../index";
 import ConditionsInfo from "../ConditionsInfo";
 import TempInfo from "../TempInfo";
 import { ArticleWrapper, Info, WeatherTileSection } from "../styled";
+import Button from "../../Button";
+import HourlyInfo from "../HourlyInfo";
+import { useDispatch } from "react-redux";
+import { setAdditionalContentStatus } from "../../../features/weatherSlice";
 
 const ForecastTile = ({ data, savedInFav, favOnClick, forecastData }) => {
+  const dispatch = useDispatch();
+
   return (
     <WeatherTile
       title={data.location.name}
       savedInFav={savedInFav}
       favOnClick={favOnClick}
-      tileView
       forecast="true"
     >
-      {forecastData &&
-        forecastData.map((forecast, index) => (
-          <ArticleWrapper additionalArticleWrapper key={index}>
-            <WeatherTileSection>
-              <Info bold>
-                {new Intl.DateTimeFormat("en", { weekday: "long" }).format(
-                  forecast.date_epoch * 1000
-                )}
-              </Info>
-              <Info>{forecast.date.split("-").reverse().join(".")}</Info>
-            </WeatherTileSection>
-            <ConditionsInfo
-              secondary="true"
-              wind={forecast.day.maxwind_kph}
-              humidity={forecast.day.avghumidity}
-              children={
-                <>
-                  <TempInfo
-                    temp={forecast.day.maxtemp_c.toFixed(0)}
-                    iconCode={forecast.day.condition.code}
-                    isDay={1}
-                    secondary="true"
-                  />
-                  <TempInfo
-                    temp={forecast.day.mintemp_c.toFixed(0)}
-                    iconCode={forecast.day.condition.code}
-                    isDay={0}
-                    secondary="true"
-                  />
-                </>
-              }
-            />
-          </ArticleWrapper>
-        ))}
+      <ArticleWrapper tileView>
+        {forecastData &&
+          forecastData.map((forecast, index) => (
+            <ArticleWrapper tileArticle key={index}>
+              <ArticleWrapper internalArticleWrapper>
+                <WeatherTileSection>
+                  <Info bold>
+                    {new Intl.DateTimeFormat("en", { weekday: "long" }).format(
+                      forecast.date_epoch * 1000
+                    )}
+                  </Info>
+                  <Info>{forecast.date.split("-").reverse().join(".")}</Info>
+                </WeatherTileSection>
+                <ConditionsInfo
+                  secondary="true"
+                  wind={forecast.day.maxwind_kph}
+                  humidity={forecast.day.avghumidity}
+                  visibility={forecast.day.avgvis_km.toFixed(1)}
+                  children={
+                    <>
+                      <TempInfo
+                        temp={forecast.day.maxtemp_c.toFixed(0)}
+                        iconCode={100}
+                        secondary="true"
+                      />
+                      <TempInfo
+                        temp={forecast.day.mintemp_c.toFixed(0)}
+                        iconCode={200}
+                        secondary="true"
+                      />
+                    </>
+                  }
+                />
+              </ArticleWrapper>
+              <HourlyInfo
+                hourlyData={forecast.hour}
+                hourlyForecast={forecast.isAdditionalContentClosed}
+              />
+              <Button
+                articleButton
+                iconDown={forecast.isAdditionalContentClosed}
+                handleOnClick={() =>
+                  dispatch(setAdditionalContentStatus(index))
+                }
+              />
+            </ArticleWrapper>
+          ))}
+      </ArticleWrapper>
     </WeatherTile>
   );
 };

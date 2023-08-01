4a525301511470fb9627840ef53feb8905b482c1
Patryk Krawczyk <patrick36212@gmail.com>
2023-08-01 15:04:20 +0200

    change fns for localtime_epoch to fix dates problems

--- src/components/WeatherTile/WeatherTileHeading/Date/index.js
+++ src/components/WeatherTile/WeatherTileHeading/Date/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import { DateWrapper, DateInfo } from "./styled";
 
 const DateComponent = ({ localtime }) => {
-  const timestamp = new Date(localtime);
+  const timestamp = localtime * 1000;
   const day = new Intl.DateTimeFormat("en", { weekday: "long" }).format(
     timestamp
   );
-  const fullDateFormat = timestamp.toLocaleDateString("pl", {
+  const fullDateFormat = new Date(timestamp).toLocaleDateString("pl", {
     month: "2-digit",
     day: "2-digit",
   });

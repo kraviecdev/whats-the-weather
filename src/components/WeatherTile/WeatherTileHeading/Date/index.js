7081b88ec3dc8048c18e3badc85b7123f0fa60fe
Patryk Krawczyk <patrick36212@gmail.com>
2023-08-04 17:50:29 +0200

    del double multiplication from date

--- src/components/WeatherTile/WeatherTileHeading/Date/index.js
+++ src/components/WeatherTile/WeatherTileHeading/Date/index.js
@@ -2,11 +2,10 @@ import React from "react";
 import { DateWrapper, DateInfo } from "./styled";
 
 const DateComponent = ({ localtime }) => {
-  const timestamp = localtime * 1000;
   const day = new Intl.DateTimeFormat("en", { weekday: "long" }).format(
-    timestamp
+    localtime
   );
-  const fullDateFormat = new Date(timestamp).toLocaleDateString("pl", {
+  const fullDateFormat = new Date(localtime).toLocaleDateString("pl", {
     month: "2-digit",
     day: "2-digit",
   });

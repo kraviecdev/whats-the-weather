d3143c3fb0337f7ab83fbe6c6696c530e087cf9a
Patryk Krawczyk <patrick36212@gmail.com>
2023-08-05 16:04:39 +0200

    add div for left/right touch events add props to it

--- src/components/WeatherTile/CurrentTile/index.js
+++ src/components/WeatherTile/CurrentTile/index.js
@@ -4,8 +4,15 @@ import TempInfo from "../TempInfo";
 import ConditionsInfo from "../ConditionsInfo";
 import { WeatherTileSection } from "../styled";
 import HourlyInfo from "../HourlyInfo";
+import React from "react";
 
-const CurrentTile = ({ data, favOnClick, savedInFav, hourlyData }) => {
+const CurrentTile = ({
+  data,
+  favOnClick,
+  savedInFav,
+  hourlyData,
+  touchHandlers,
+}) => {
   const localtime = data.location.localtime_epoch * 1000;
   const currentHour = new Date(localtime).getHours();
 
@@ -18,23 +25,25 @@ const CurrentTile = ({ data, favOnClick, savedInFav, hourlyData }) => {
     >
       <Slider />
 
-      <WeatherTileSection>
-        <TempInfo
-          temp={data.current.temp_c.toFixed(0)}
-          iconCode={data.current.condition.code}
-          isDay={data.current.is_day}
-          conditionText={data.current.condition.text}
-          main="true"
-        />
-      </WeatherTileSection>
+      <div {...touchHandlers}>
+        <WeatherTileSection>
+          <TempInfo
+            temp={data.current.temp_c.toFixed(0)}
+            iconCode={data.current.condition.code}
+            isDay={data.current.is_day}
+            conditionText={data.current.condition.text}
+            main="true"
+          />
+        </WeatherTileSection>
 
-      <ConditionsInfo
-        basic="true"
-        feelsLike={data.current.feelslike_c.toFixed(0)}
-        pressure={data.current.pressure_mb}
-        humidity={data.current.humidity}
-        wind={data.current.gust_kph}
-      />
+        <ConditionsInfo
+          basic="true"
+          feelsLike={data.current.feelslike_c.toFixed(0)}
+          pressure={data.current.pressure_mb}
+          humidity={data.current.humidity}
+          wind={data.current.gust_kph}
+        />
+      </div>
 
       <HourlyInfo hourlyData={hourlyData} currentHour={currentHour} />
     </WeatherTile>

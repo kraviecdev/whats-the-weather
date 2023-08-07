0bb48becf910ecbc7885f65832d6d4e3e79c1366
Patryk Krawczyk <patrick36212@gmail.com>
2023-08-07 17:52:07 +0200

    create & style Error Button

--- src/components/StatusInfo/Error/styled.js
+++ src/components/StatusInfo/Error/styled.js
@@ -1,5 +1,6 @@
 import { ReactComponent as Error } from "./sad_cloud.svg";
 import styled, { css } from "styled-components";
+import { NavLink } from "react-router-dom";
 export const ErrorIcon = styled(Error)`
   fill: ${({ theme }) => theme.colors.secondaryFont};
   stroke: ${({ theme }) => theme.colors.secondaryFont};
@@ -33,4 +34,21 @@ export const ErrorInfo = styled.p`
       font-weight: 700;
       color: ${({ theme }) => theme.colors.errorColor};
     `}
+`;
+
+export const ErrorButton = styled(NavLink)`
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 6px;
+  padding: 12px 32px;
+  border-radius: 16px;
+  text-decoration: none;
+  font-weight: 700;
+  box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.25);
+  color: ${({ theme }) => theme.colors.mainFont};
+  background: ${({ theme }) => theme.colors.secondaryColor};
+  width: max-content;
 `;

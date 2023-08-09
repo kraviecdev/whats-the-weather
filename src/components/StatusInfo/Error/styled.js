1d5c1afdcc8364f396ff9ac214f2283544958169
Patryk Krawczyk <patrick36212@gmail.com>
2023-08-09 20:11:06 +0200

    delete common styled components to main styled

--- src/components/StatusInfo/Error/styled.js
+++ src/components/StatusInfo/Error/styled.js
@@ -1,39 +1,10 @@
 import { ReactComponent as Error } from "./sad_cloud.svg";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 export const ErrorIcon = styled(Error)`
   fill: ${({ theme }) => theme.colors.secondaryFont};
   stroke: ${({ theme }) => theme.colors.secondaryFont};
   margin: 0 0 167px 0;
-`;
-
-export const ErrorInfoWrapper = styled.div`
-  position: absolute;
-  bottom: 0;
-  max-width: 1024px;
-  width: 100%;
-  padding: 16px;
-  border-radius: 16px 16px;
-  background: linear-gradient(
-    180deg,
-    ${({ theme }) => theme.colors.secondaryColor} 0%,
-    ${({ theme }) => theme.colors.mainColor} 100%
-  );
-  display: grid;
-  justify-items: center;
-  grid-gap: 8px;
-`;
-export const ErrorInfo = styled.p`
-  font-size: 16px;
-  text-align: center;
-  color: ${({ theme }) => theme.colors.mainFont};
-
-  ${({ contrast }) =>
-    contrast &&
-    css`
-      font-weight: 700;
-      color: ${({ theme }) => theme.colors.errorColor};
-    `}
 `;
 
 export const ErrorButton = styled(NavLink)`

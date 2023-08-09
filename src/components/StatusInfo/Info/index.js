5c109a7b80b4dc90bb7918930fadbade13c12456
Patryk Krawczyk <patrick36212@gmail.com>
2023-08-09 20:10:05 +0200

    create Info Component

---
+++ src/components/StatusInfo/Info/index.js
@@ -0,0 +1,38 @@
+import { StatusHeader, StatusInfo, StatusInfoWrapper } from "../styled";
+import { UpIcon } from "../../Button/styled";
+import { useState } from "react";
+
+const Info = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  return (
+    <>
+      <div>
+        <StatusHeader>Hello there!</StatusHeader>
+        <StatusInfo>
+          For the weather forecast, enter the name of the city up there
+        </StatusInfo>
+      </div>
+      <StatusInfoWrapper
+        hidden="true"
+        isClicked={isClicked}
+        onClick={() => setIsClicked(!isClicked)}
+      >
+        <StatusHeader>
+          Geolocation Info <UpIcon />
+        </StatusHeader>
+        <StatusInfo>
+          We respect your privacy, that's why we do not collect any data from
+          you and your device.
+        </StatusInfo>
+        <StatusInfo>
+          However, if you want weather forecast for your current position, we
+          need your permission for geolocation. You can change it in you browser
+          settings.
+        </StatusInfo>
+      </StatusInfoWrapper>
+    </>
+  );
+};
+
+export default Info;

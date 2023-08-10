89e81d3c967e0002dc6767e18689e1d2a03e95b8
Patryk Krawczyk <patrick36212@gmail.com>
2023-08-10 14:28:22 +0200

    refactor for more common name

--- src/features/getWeatherData.js
+++ src/features/getWeatherData.js
@@ -1,6 +1,6 @@
 import { fetchFromApi } from "../core/fetchFromApi";
 
-export const getCurrentData = (coordinates, days) =>
+export const getWeatherData = (coordinates, days) =>
   fetchFromApi({
     path: "/forecast.json",
     params: {

f266ff2967f228c1037d52ea9f347e0e039d8993
Patryk Krawczyk <patrick36212@gmail.com>
2023-08-13 18:48:29 +0200

    rename path Add last path to tile view for all searches

--- src/core/routes.js
+++ src/core/routes.js
@@ -6,15 +6,18 @@ import {
 } from "react-router-dom";
 import WeatherPage from "../features/WeatherPage";
 import MainPage from "../features/MainPage";
+import CitiesPage from "../features/CitiesPage";
 
 export const toMainPage = "/weather";
-const toCurrentCityWeather = "/weather/:city";
+const toWeatherPage = "/weather/:city";
+const toCitiesPage = "/cities";
 
 export const router = createHashRouter(
   createRoutesFromElements(
     <>
       <Route path={toMainPage} element={<MainPage />} />
-      <Route path={toCurrentCityWeather} element={<WeatherPage />} />
+      <Route path={toWeatherPage} element={<WeatherPage />} />
+      <Route path={toCitiesPage} element={<CitiesPage />} />
       <Route path="/" element={<Navigate to={toMainPage} />} />
     </>
   )

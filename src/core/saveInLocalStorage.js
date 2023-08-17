3f343793ffea671edd993cfd487bcaed0dfa263b
Patryk Krawczyk <patrick36212@gmail.com>
2023-08-17 10:29:20 +0200

    change prop name

--- src/core/saveInLocalStorage.js
+++ src/core/saveInLocalStorage.js
@@ -1,7 +1,9 @@
 const localStorageKey = "cities";
 
 export const saveSearchesInLocalStorage = (doneSearches) => {
-  const favouritesSearches = doneSearches.filter(({ fav }) => fav === true);
+  const favouritesSearches = doneSearches.filter(
+    ({ favourite }) => favourite === true
+  );
   localStorage.setItem(localStorageKey, JSON.stringify(favouritesSearches));
 };
 export const getSearchesFromLocalStorage = () =>

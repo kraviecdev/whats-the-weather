5a1b38fc7c5b48566ba364c3622c2dde7e72cd6e
Patryk Krawczyk <patrick36212@gmail.com>
2023-08-17 08:53:04 +0200

    place navigator fn in the app component

--- src/core/App.js
+++ src/core/App.js
@@ -5,15 +5,35 @@ import { day } from "../themes/theme";
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import Loading from "../components/StatusInfo/Loading";
+import { setGeoAgreement, setGeoCoordinates } from "../features/weatherSlice";
+import { useDispatch } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
   const [loadingScreen, setLoadingScreen] = useState(false);
 
   useEffect(() => {
     setLoadingScreen(true);
+
     setTimeout(() => {
-      setLoadingScreen(false);
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          dispatch(setGeoAgreement(true));
+          dispatch(
+            setGeoCoordinates({
+              lat: position.coords.latitude,
+              lon: position.coords.longitude,
+            })
+          );
+          setLoadingScreen(false);
+        },
+        () => {
+          dispatch(setGeoAgreement(false));
+          setLoadingScreen(false);
+        }
+      );
     }, 1000);
+    // eslint-disable-next-line
   }, []);
 
   return (

61d57242afaa3333f288785fcf2cb5def845de94
Patryk Krawczyk <patrick36212@gmail.com>
2023-08-21 21:18:34 +0200

    delete weatherTile Component, prop name change from basic to main

--- src/components/WeatherTile/CurrentTile/index.js
+++ src/components/WeatherTile/CurrentTile/index.js
@@ -1,10 +1,10 @@
+import React from "react";
+import WeatherTileHeading from "../WeatherTileHeading";
 import Slider from "../Slider";
-import WeatherTile from "../index";
 import TempInfo from "../TempInfo";
 import ConditionsInfo from "../ConditionsInfo";
-import { WeatherTileSection } from "../styled";
 import HourlyInfo from "../HourlyInfo";
-import React from "react";
+import { ArticleWrapper, WeatherTileSection } from "../styled";
 
 const CurrentTile = ({
   data,
@@ -17,36 +17,42 @@ const CurrentTile = ({
   const currentHour = new Date(data.location.localtime).getHours();
 
   return (
-    <WeatherTile
-      localtime={localtime}
-      title={data.location.name}
-      favOnClick={favOnClick}
-      savedInFav={savedInFav}
-    >
+    <ArticleWrapper>
+      <WeatherTileHeading
+        title={data.location.name}
+        localtime={localtime}
+        favOnClick={favOnClick}
+        savedInFav={savedInFav}
+      />
+
       <Slider />
 
       <div {...touchHandlers}>
         <WeatherTileSection>
           <TempInfo
+            main
             temp={data.current.temp_c.toFixed(0)}
             iconCode={data.current.condition.code}
             isDay={data.current.is_day}
             conditionText={data.current.condition.text}
-            main="true"
           />
         </WeatherTileSection>
 
-        <ConditionsInfo
-          basic="true"
-          feelsLike={data.current.feelslike_c.toFixed(0)}
-          pressure={data.current.pressure_mb}
-          humidity={data.current.humidity}
-          wind={data.current.gust_kph}
-        />
+        <WeatherTileSection additionalContentSection borderBottom>
+          <ConditionsInfo
+            main
+            feelsLike={data.current.feelslike_c.toFixed(0)}
+            pressure={data.current.pressure_mb}
+            humidity={data.current.humidity}
+            wind={data.current.gust_kph}
+          />
+        </WeatherTileSection>
       </div>
 
-      <HourlyInfo hourlyData={hourlyData} currentHour={currentHour} />
-    </WeatherTile>
+      <WeatherTileSection hourlyContentSection>
+        <HourlyInfo hourlyData={hourlyData} currentHour={currentHour} />
+      </WeatherTileSection>
+    </ArticleWrapper>
   );
 };
 

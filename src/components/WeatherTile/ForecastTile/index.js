55150213b0148f3e98c3dda16c8b837628198005
Patryk Krawczyk <patrick36212@gmail.com>
2023-08-21 21:23:34 +0200

    change forecast section structure for better semantic and css

--- src/components/WeatherTile/ForecastTile/index.js
+++ src/components/WeatherTile/ForecastTile/index.js
@@ -1,9 +1,15 @@
-import WeatherTile from "../index";
+import Section from "../../Section";
+import WeatherTileHeading from "../WeatherTileHeading";
 import ConditionsInfo from "../ConditionsInfo";
 import TempInfo from "../TempInfo";
-import { ArticleWrapper, Info, WeatherTileSection } from "../styled";
 import Button from "../../Button";
 import HourlyInfo from "../HourlyInfo";
+import {
+  ArticleWrapper,
+  Info,
+  WeatherTileSection,
+  WeatherTileWrapper,
+} from "../styled";
 import { useDispatch } from "react-redux";
 import { setContentHidden } from "../../../features/weatherSlice";
 
@@ -11,60 +17,70 @@ const ForecastTile = ({ data, savedInFav, favOnClick, forecastData }) => {
   const dispatch = useDispatch();
 
   return (
-    <WeatherTile
-      title={data.location.name}
-      savedInFav={savedInFav}
-      favOnClick={favOnClick}
-      additionalInfo="Forecast"
-      forecastArticleWrapper="true"
-    >
-      <ArticleWrapper tileView>
+    <Section>
+      <WeatherTileHeading
+        title={data.location.name}
+        additionalInfo="Forecast"
+        favOnClick={favOnClick}
+        savedInFav={savedInFav}
+      />
+
+      <Section tileView>
         {forecastData &&
           forecastData.map((forecast, index) => (
             <ArticleWrapper tileArticle key={index}>
-              <ArticleWrapper internalArticleWrapper>
-                <WeatherTileSection>
+              <Section rowGap>
+                <WeatherTileWrapper noPadding additionalInfoWrapper>
                   <Info bold>
                     {new Intl.DateTimeFormat("en", { weekday: "long" }).format(
                       forecast.date_epoch * 1000
                     )}
                   </Info>
                   <Info>{forecast.date.split("-").reverse().join(".")}</Info>
+                </WeatherTileWrapper>
+
+                <WeatherTileSection additionalContentSection>
+                  <ConditionsInfo
+                    secondary
+                    wind={forecast.day.maxwind_kph}
+                    humidity={forecast.day.avghumidity}
+                    visibility={forecast.day.avgvis_km.toFixed(1)}
+                    children={
+                      <>
+                        <TempInfo
+                          temp={forecast.day.maxtemp_c.toFixed(0)}
+                          iconCode={100}
+                          secondary="true"
+                        />
+                        <TempInfo
+                          temp={forecast.day.mintemp_c.toFixed(0)}
+                          iconCode={200}
+                          secondary="true"
+                        />
+                      </>
+                    }
+                  />
                 </WeatherTileSection>
-                <ConditionsInfo
-                  secondary="true"
-                  wind={forecast.day.maxwind_kph}
-                  humidity={forecast.day.avghumidity}
-                  visibility={forecast.day.avgvis_km.toFixed(1)}
-                  children={
-                    <>
-                      <TempInfo
-                        temp={forecast.day.maxtemp_c.toFixed(0)}
-                        iconCode={100}
-                        secondary="true"
-                      />
-                      <TempInfo
-                        temp={forecast.day.mintemp_c.toFixed(0)}
-                        iconCode={200}
-                        secondary="true"
-                      />
-                    </>
-                  }
-                />
-              </ArticleWrapper>
-              <HourlyInfo
-                hourlyData={forecast.hour}
-                hourlyForecast={forecast.isContentHidden}
-              />
+              </Section>
+
+              <WeatherTileSection
+                hourlyContentSection
+                contentHidden={forecast.isContentHidden}
+              >
+                <HourlyInfo hourlyData={forecast.hour} />
+              </WeatherTileSection>
+
               <Button
                 articleButton
                 iconDown={forecast.isContentHidden}
-                handleOnClick={() => dispatch(setContentHidden(index))}
+                handleOnClick={() =>
+                  dispatch(setContentHidden({ cities: false, index: index }))
+                }
               />
             </ArticleWrapper>
           ))}
-      </ArticleWrapper>
-    </WeatherTile>
+      </Section>
+    </Section>
   );
 };
 

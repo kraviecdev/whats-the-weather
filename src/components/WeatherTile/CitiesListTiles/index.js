b9b7e58ef58972b0b53141b509febb78d1c9b390
Patryk Krawczyk <patrick36212@gmail.com>
2023-09-04 20:32:53 +0200

    create CitiesListTiles component more complex, change props to be connected with objects values

--- src/components/WeatherTile/CitiesListTiles/index.js
+++ src/components/WeatherTile/CitiesListTiles/index.js
@@ -1,45 +1,74 @@
-import { ArticleWrapper } from "../styled";
+import { setContentHidden } from "../../../features/weatherSlice";
+import { selectDoneSearches, toggleFavourite } from "../../Search/searchSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { ArticleWrapper, WeatherTileSection } from "../styled";
 import Button from "../../Button";
+import Section from "../../Section";
 import WeatherTileHeading from "../WeatherTileHeading";
 import TempInfo from "../TempInfo";
 import ConditionsInfo from "../ConditionsInfo";
 import HourlyInfo from "../HourlyInfo";
 
-const CitiesListTiles = ({ cities, handleOnClick, favOnClick }) => {
+const CitiesListTiles = ({ cities }) => {
+  const dispatch = useDispatch();
+  const doneSearches = useSelector(selectDoneSearches);
+
   return (
-    <ArticleWrapper tileView>
+    <Section tileView citiesSection>
       {cities &&
         cities.map((city, index) => (
           <ArticleWrapper tileArticle key={index}>
-            <ArticleWrapper internalArticleWrapper>
+            <Section rowGap>
               <WeatherTileHeading
-                savedInFav={true}
+                secondary
+                savedInFav={
+                  doneSearches.find((search) => search.id === city.id).favourite
+                }
                 title={city.location.name}
-                favOnClick={favOnClick}
+                additionalInfo={city.location.country}
+                favOnClick={() => dispatch(toggleFavourite(city.id))}
               />
+
               <TempInfo
                 secondary="true"
                 iconCode={city.current.condition.code}
                 isDay={city.current.is_day}
-                temp={city.current.temp_c}
+                temp={city.current.temp_c.toFixed(0)}
               />
-            </ArticleWrapper>
-            <ConditionsInfo
-              basic="true"
-              wind={city.current.gust_kph}
-              humidity={city.current.humidity}
-              pressure={city.current.pressure_mb}
-              feelsLike={city.current.feelslike_c}
-            />
-            <HourlyInfo hourlyData={city.hourlyWeather} />
+            </Section>
+
+            <WeatherTileSection
+              citiesSection
+              contentHidden={city.isContentHidden}
+            >
+              <WeatherTileSection additionalContentSection borderBottom>
+                <ConditionsInfo
+                  main
+                  pressure={city.current.pressure_mb}
+                  wind={city.current.wind_kph}
+                  humidity={city.current.humidity}
+                  feelsLike={city.current.feelslike_c.toFixed(0)}
+                />
+              </WeatherTileSection>
+
+              <WeatherTileSection hourlyContentSection>
+                <HourlyInfo
+                  currentHour={new Date(city.location.localtime).getHours()}
+                  hourlyData={city.forecast}
+                />
+              </WeatherTileSection>
+            </WeatherTileSection>
+
             <Button
               articleButton
-              iconDown={city.isAdditionalContentVisible}
-              handleOnClick={handleOnClick}
+              iconDown={city.isContentHidden}
+              handleOnClick={() =>
+                dispatch(setContentHidden({ cities: true, index: index }))
+              }
             />
           </ArticleWrapper>
         ))}
-    </ArticleWrapper>
+    </Section>
   );
 };
 

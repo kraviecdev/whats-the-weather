37c9997e6818a5d1bffd3334a64633b121185773
patrick36212 <patrick36212@gmail.com>
2024-02-06 09:47:36 +0100

    implement API_KEY as env variable

--- src/core/fetchFromApi.js
+++ src/core/fetchFromApi.js
@@ -1,7 +1,6 @@
 import { buildQueryString } from "./buildQueryString";
-import { WEATHER_API_KEY } from "./WEATHER_API_KEY";
 
-const API_KEY = WEATHER_API_KEY;
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 const API_URL = "https://api.weatherapi.com/v1";
 
 const timeout = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
